01906d4941a954811d5117100ff217b3cacaf1b9
keisuke-yanagisawa <yanagisawa@bi.cs.titech.ac.jp>
2016-06-11 09:56:30 +0900

    同じ評価値になってしまった場合の対処を実施中。まだうまく動いていない。

--- ef.py
+++ ef.py
@@ -1,6 +1,7 @@
 # coding: UTF-8 
 
 import pandas as pd
+import numpy as np
 
 def ef(values, labels, ratio, definition=(0,1)):
     """
@@ -20,43 +21,37 @@ def ef(values, labels, ratio, definition=(0,1)):
         return None
     labels = [definition.index(x) for x in labels]
 
+    ratio1 = np.sum(labels, dtype=float)/len(labels)
 
     df = pd.DataFrame.from_dict({"value": values, "label": labels})
     df = df.sort_values("value", ascending=False).reset_index();
     del df["index"]
-    
-    df.label = df.label.astype(float)
 
-    all_ratio = sum(df.label) / len(df)
-    index = int(len(df)*ratio)
+    meaned_label_df = df.groupby("value").mean().reset_index();
+    df = pd.merge(df[["value"]], meaned_label_df, on="value", how="left")
+
+    index = int(len(labels)*ratio)
     partial_df = df[:index]
-
-    meaned_label = df[df.value == df.loc[index-1].value].label.mean()
-    df.loc[df.value == df.loc[index-1].value, "label"] = meaned_label
-
-    partial_df = df[:int(len(df)*ratio)] # refresh
-    par_ratio = sum(partial_df.label) / len(partial_df)
-    ef = par_ratio / all_ratio;
+    par_ratio = float(sum(partial_df.label)) / index
+    ef = par_ratio / ratio1;
     print ef
     return ef
 
 def test(cond, statement_dict):
     if cond:
         print("A test is passed.")
-        for key, value in statement_dict.iteritems():
-            print(key, value)
+    else:
+        print("A test is failed.")
+    for key, value in statement_dict.iteritems():
+        print(key, value)
 
 if __name__ == '__main__':
-    values = [1, 2, 3, 4, 5]
-    labels = [0, 0, 1, 0, 1]
-    
     case1 = {
-        "values": values,
-        "labels": labels,
+        "values": [1, 2, 3, 4, 5],
+        "labels": [0, 0, 1, 0, 1],
         "ratio" : 0.2,
-        "result": 2.5
+        "result": 2.5,
     }
-
     test(ef(case1["values"], case1["labels"], case1["ratio"]) == case1["result"], case1)
 
 
@@ -64,3 +59,10 @@ if __name__ == '__main__':
     case2["definition"] = (1,0)
     case2["result"] = 0
     test(ef(case2["values"], case2["labels"], case2["ratio"], definition=case2["definition"]) == case2["result"], case2)
+
+    #TODO the result of case3 is wrong.
+    case3 = case2
+    case3["values"] = [1,2,4,5,5]
+    case3["ratio"] = 0.4
+    case3["result"] = (1.0/2) / (2.0/5)
+    test(ef(case3["values"], case3["labels"], case3["ratio"], definition=case3["definition"]) == case3["result"], case3)
