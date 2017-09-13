65aa6e928781c6a09770d5cbf635aaed4b0e43ed
Keisuke Yanagisawa <kyanagis@purdue.edu>
2017-09-13 15:29:21 -0400

    [FIX] to be readable format

--- compound.py
+++ compound.py
@@ -1,6 +1,13 @@
 ### Manipulate compound files like mol2, sdf, pdb and so on.
 
 import re
+from rdkit import Chem
 
 def separateMol2Blocks(string):
-    return re.compile("\n(?=@\<TRIPOS\>MOLECULE)").split(string)
+
+    # separate
+    mol2blocks = re.compile("\n(?=@\<TRIPOS\>MOLECULE)").split(string)
+
+    # add \n for all blocks except for the last block
+    mol2blocks[-1] = mol2blocks[-1][:-1]
+    return [mol2block+'\n' for mol2block in mol2blocks]
