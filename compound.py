806681310ef69f46ca6593265b6568e2af642a54
Keisuke Yanagisawa <kyanagis@purdue.edu>
2017-09-13 16:39:51 -0400

    [add] several methods to be similar to rdkit functions read sdf file

--- compound.py
+++ compound.py
@@ -1,5 +1,4 @@
 ### Manipulate compound files like mol2, sdf, pdb and so on.
-
 import re
 from rdkit import Chem
 
@@ -11,3 +10,33 @@ def separateMol2Blocks(string):
     # add \n for all blocks except for the last block
     mol2blocks[-1] = mol2blocks[-1][:-1]
     return [mol2block+'\n' for mol2block in mol2blocks]
+
+def MOL2BlockSupplier(filename):
+    with open(filename) as fin:
+        ForwardMol2BlockSupplier(fin)
+
+def ForwardMol2BlockSupplier(fileobj):
+    mol2block = fileobj.readline()
+    if not mol2block:
+        print 'There is not any lines in the file: %s'%filename
+        exit
+        
+    line = fileobj.readline()
+    while line:
+        if line.startswith('@<TRIPOS>MOLECULE'):
+            yield mol2block
+            mol2block = line
+        else:
+            mol2block = mol2block + line
+        line = fileobj.readline()
+    yield mol2block
+
+def Mol2MolSupplier(filename, sanitize=True, removeHs=True):
+    suppl = Mol2BlockSupplier(filename)
+    for block in suppl:
+        yield Chem.MolFromMol2Block(block, sanitize, removeHs)
+
+def ForwardMol2MolSupplier(fileobj, sanitize=True, removeHs=True):
+    suppl = ForwardMol2BlockSupplier(fileobj)
+    for block in suppl:
+        yield Chem.MolFromMol2Block(block, sanitize, removeHs)
