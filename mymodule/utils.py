1eadb015702853682ef655c319e0b573c47d4efa
Keisuke YANAGISAWA <yanagisawa@bi.a.u-tokyo.ac.jp>
2020-02-24 17:14:41 +0900

    utils.pyを補強

--- mymodule/utils.py
+++ mymodule/utils.py
@@ -1,33 +1,47 @@
+# -*- coding: utf-8 -*-
 import configparser
 import os
 
 
-def assert_config(dat):
-    None
+def read_config(filepath):
+    """
+    Configファイルを読み込む。
 
+    Parameters
+    --------
+    filepath : str
+        Configファイルのパス。
 
-def init_config():
-    aDict = {
-        "InputDir": {
-        },
-        "OutputDir": {
-            "temporal": "/tmp/deepsitecrypto",
-        },
-        "Executables": {
-            "blastp": "blastp",
-            "makeblastdb": "makeblastdb",
-        },
-    }
+    Returns
+    --------
+    dat : configparser.ConfigParser
+        Configファイルを読み込んだ結果のオブジェクト。
+
+    Raises
+    --------
+    FileNotFoundError
+        filepath にファイルが存在しない場合に発生。
+    """
+
     dat = configparser.ConfigParser()
-    dat.read_dict(aDict)
+    with open(filepath) as fin:
+        dat.read_file(fin)
     return dat
 
 
-def read_config(configfile):
-    dat = init_config()
-    if not os.path.exists(configfile):
-        print("[ERROR]", configfile, "is not exist")
-        exit(1)
-    dat.read(configfile)
-    assert_config(dat)
-    return dat
+def TEST_read_config():
+    os.system("touch .test.conf")
+    dat = read_config(".test.conf")
+    assert(type(dat) == configparser.ConfigParser)
+
+    os.system("touch .notexist.conf; rm .notexist.conf")
+    try:
+        dat2 = read_config(".notexist.conf")
+    except FileNotFoundError:
+        None
+    else:
+        assert(False)  # FileNotFoundError should be happened
+
+
+if __name__ == "__main__":
+    TEST_read_config()
